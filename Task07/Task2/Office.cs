4c4b01ec0835e67863c0144d96d5d0d36630c41b
TottoroChan <kototottoro@gmail.com>
2016-02-22 21:11:59 +0400

    Исправлено второе  задание

---
+++ Task07/Task2/Office.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task2
+{
+	class Office
+	{
+		public string personName { get; set; }
+
+		/// <summary>
+		///  конструктор, создающй содрудника с заданным именем 
+		/// </summary>
+		public Office(string name)
+		{
+			personName = name;
+		}
+
+		/// <summary>
+		/// Сотрудник приходит на работу
+		/// </summary>
+		public static void CameToWork(Office name)
+		{
+			Console.WriteLine("[На работу пришёл {0}]", name);
+
+			if (OnCameToWork != null)
+				OnCameToWork(name);
+
+			OnCameToWork += name.Greeting;
+			OnGoHome += name.Farewell;
+
+			Console.WriteLine();
+		}
+
+		/// <summary>
+		/// Сотрудник уходит домой
+		/// </summary>
+		public static void GoHome(Office name)
+		{
+			Console.WriteLine("[{0} ушёл домой.]", name);
+
+			OnCameToWork -= name.Greeting;
+			OnGoHome -= name.Farewell;
+
+			if (OnGoHome != null)
+				OnGoHome(name);
+
+			Console.WriteLine();
+		}
+
+		/// <summary>
+		/// Приветствие в зависимости от текущего времени.
+		/// </summary>
+		public void Greeting(Office anotherPerson)
+		{
+			string greet;
+			if (DateTime.Now.Hour < 12)
+				greet = "Доброе утро";
+			else if (DateTime.Now.Hour < 17)
+				greet = "Добрый день";
+			else greet = "Добрый вечер";
+
+			Console.WriteLine("'{0}, {1}!', - сказал {2}.", greet, anotherPerson, this);
+		}
+
+		/// <summary>
+		///  Прощание
+		/// </summary>
+		public void Farewell(Office anotherPerson)
+		{
+			Console.WriteLine("'До свидания, {0}!', - сказал {1}.", anotherPerson, this);
+		}
+
+		//События 
+		public delegate void Message (Office name);
+		public static event Message OnCameToWork;
+		public static event Message OnGoHome;
+
+		//переопределение ToString
+		public override string ToString()
+		{
+			return personName;
+		}
+	}
+}
--- Task07/Task2/Program.cs
+++ Task07/Task2/Program.cs
@@ -6,40 +6,16 @@ namespace Task2
     {
 		static void Main(string[] args)
         {
-			TimeEvent T = new TimeEvent();
+			Office jhon = new Office("Джон");
+			Office hugo = new Office("Хьюго");
+			Office bill = new Office("Билл");
 
-			Person jhon = new Person ("Джон");
-			Message greetingByJhon = new Message(jhon.Greeting);
-
-			Person hugo = new Person ("Хьюго");
-			Message greetingByHugo = new Message(hugo.Greeting);
-			Message greetingByUs = greetingByJhon;
-			if (greetingByUs != null)
-				greetingByUs("Хьюго");
-			Message farewellByHugo = new Message(hugo.Farewell);
-			Message farewellByUs = farewellByHugo; 
-
-			Person bill = new Person ("Билл");
-			Message greetingByBill = new Message(bill.Greeting);
-			greetingByUs += greetingByHugo;
-			if (greetingByUs != null)
-				greetingByUs("Билл");
-			Message farewellByBill = new Message(bill.Farewell);
-			farewellByUs += farewellByBill;
-
-			jhon.GoHome();
-			if (farewellByUs != null)
-				farewellByUs("Джон");
-
-			hugo.GoHome();
-			farewellByUs -= farewellByHugo;
-			if (farewellByUs != null)
-				farewellByUs("Хьюго");
-
-			bill.GoHome();
-			farewellByUs -= farewellByBill;
-			if (farewellByUs != null)
-				farewellByUs("Билл");
+			Office.CameToWork(jhon);
+			Office.CameToWork(hugo);
+			Office.CameToWork(bill);
+			Office.GoHome(jhon);
+			Office.GoHome(hugo);
+			Office.GoHome(bill);
 
 			Console.Write("Нажмите любую клавишу для закрытия программы.");
             Console.ReadKey();
