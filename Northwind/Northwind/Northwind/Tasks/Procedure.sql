df3b5a1bcfe80dd74ff816c69323947edee32c67
TottoroChan <kototottoro@gmail.com>
2016-03-12 20:29:21 +0400

    13,1 13,2

---
+++ Northwind/Northwind/Northwind/Tasks/Procedure.sql
@@ -0,0 +1,36 @@
+﻿USE Northwind;
+GO
+--13.1
+CREATE PROCEDURE Northwind.GreatestOrders
+	@top int,
+	@year int
+AS
+BEGIN
+SELECT TOP(@top) OrderID, e.FirstName + ' '+ e.LastName AS Person,  Price 
+FROM Northwind.Employees AS e 
+INNER JOIN 
+( SELECT odmax.EmployeeID, OrderID, MAX(odmax.Price) AS Price 
+FROM 
+( SELECT o.EmployeeID, od.OrderID AS OrderID, 
+SUM(od.Quantity * od.UnitPrice * (1 - od.Discount)) AS Price 
+FROM Northwind.[Order Details] AS od 
+INNER JOIN Northwind.orders AS o 
+ON o.OrderID = od.OrderID AND @year = YEAR(OrderDate)
+GROUP BY od.OrderID, o.EmployeeID ) AS odmax 
+GROUP BY odmax.EmployeeID, odmax.OrderID) AS max 
+ON max.EmployeeID = e.EmployeeID
+END
+GO
+
+--13.2
+CREATE PROCEDURE Northwind.ShippedOrdersDiff
+	@shippedDelay int
+AS
+BEGIN
+SELECT OrderID, CAST(OrderDate AS DATE) AS OrderDate, CAST(ShippedDate AS DATE) AS ShippedDate,
+DAY(ShippedDate - OrderDate) AS ShippedDelay
+FROM Northwind.Orders
+WHERE DAY(ShippedDate - OrderDate) > @shippedDelay OR DAY(ShippedDate - OrderDate) is null
+ORDER BY DAY(ShippedDate - OrderDate)
+END
+GO
---
+++ Northwind/Northwind/Northwind/Tasks/Query.sql
@@ -0,0 +1,29 @@
+﻿--13.1 
+EXEC Northwind.GreatestOrders 5, 1998
+
+EXEC Northwind.GreatestOrders 4, 1996
+
+--Проверочный запрос
+SELECT OrderID, e.FirstName , ' ', e.LastName AS Person,  Price 
+FROM northwind.employees AS e 
+INNER JOIN 
+( SELECT odmax.EmployeeID, OrderID, MAX(odmax.Price) AS Price 
+FROM 
+( SELECT o.EmployeeID, od.OrderID AS OrderID, 
+SUM(od.Quantity * od.UnitPrice * (1 - od.Discount)) AS Price 
+FROM Northwind.[Order Details] AS od 
+INNER JOIN Northwind.orders AS o 
+ON o.OrderID = od.OrderID 
+GROUP BY od.OrderID, o.EmployeeID ) AS odmax 
+GROUP BY odmax.EmployeeID, odmax.OrderID) AS max 
+ON max.EmployeeID = e.EmployeeID
+ORDER BY Person
+
+
+--13.2
+EXEC Northwind.ShippedOrdersDiff 35
+
+EXEC Northwind.ShippedOrdersDiff 4
+
+
+
